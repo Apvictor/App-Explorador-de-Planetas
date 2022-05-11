886958a8f80f7172249074741f14c396d825ac3f
Apvictor <armandinho14.ap@gmail.com>
2022-05-11 12:15:56 -0300

    Orientação Objeto Recebendo Lista de Planetas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Planeta } from './model/planeta';
+import { PlanetaService } from './model/planeta.service';
 
 @Component({
   selector: 'my-app',
@@ -9,30 +10,13 @@ import { Planeta } from './model/planeta';
 export class AppComponent {
   planetaSelecionado: Planeta;
 
-  planetas: Planeta[] = [
-    {
-      nome: 'Terra',
-      temperatura: 16,
-      diametro: 12.742,
-      dia: 24,
-      ano: 365,
-      curiosidade: 'Daqui a 140 milhões de anos, um dia durará 25 horas.',
-      imagem:
-        'https://drive.google.com/uc?id=1C7HmsPUqT8TkVuZ9OJlQrtG1L_Ho0LH3',
-    },
-    {
-      nome: 'Marte',
-      temperatura: 16,
-      diametro: 12.742,
-      dia: 24,
-      ano: 365,
-      curiosidade: 'Daqui a 140 milhões de anos, um dia durará 25 horas.',
-      imagem:
-        'https://drive.google.com/uc?id=1sXfwBgOihDUyN3DJjYBFsIqEpYjRLH5O',
-    },
-  ];
+  planetas: Planeta[];
 
-  ngOnInit() {}
+  constructor(private planetaService: PlanetaService) {}
+
+  ngOnInit() {
+    this.planetas = this.planetaService.getPlanetas();
+  }
 
   public clicouNoPlaneta(planeta: Planeta) {
     this.planetaSelecionado = planeta;
